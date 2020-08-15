0d23333b7b5431c8de0ca9af24d352cc2af7cfeb
Süleyman SEZER <me.iamcodder@gmail.com>
2020-08-15 23:09:38 +0300

    Added web support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_calculator/screens/splash_screen.dart';
+import 'package:flutter_calculator/screens/home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: HomeScreen(),
     );
   }
 }
